ddd841e7fd4cb171742d7ea0c6b7265bf40ac8ec
Developer Ballast Lane <ballastlane@Developers-MacBook-Pro-3.local>
2023-01-20 13:12:45 -0500

    changes in globals.css directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,12 @@ export default function Home() {
           name="description"
           content="Consigue aquí las mejores recetas para tu paladar"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link crossOrigin="true" rel="preconnect" href="https://fonts.googleapis.com" />
+        <link crossOrigin="true" rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&family=Open+Sans&display=swap"
+          crossOrigin="true"
         />
       </Head>
       <main>
