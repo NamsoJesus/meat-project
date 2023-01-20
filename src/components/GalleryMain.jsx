91ef281e9e17b58465094e206ae96f0cb99ee4ff
Developer Ballast Lane <ballastlane@Developers-MacBook-Pro-3.local>
2023-01-20 10:52:34 -0500

    hero main

--- src/components/GalleryMain.jsx
+++ src/components/GalleryMain.jsx
@@ -13,33 +13,87 @@ export const GalleryMain = () => {
         updateFilterProducts(products, e.target.value);
     }
 
+    
+  const getProducts = async () => {
+    try{
+      const resp = await axios.get(
+        `https://5eed24da4cbc340016330f0d.mockapi.io/api/articles?limit=${filterSelected}`
+      )
+        setProducts(resp.data);
+        const category = resp.data.map((product) => product.category);
+        setFilters(["Todos", ...new Set(category)]);
+        updateFilteredProducts(resp.data, filterSelected);
+    } catch (err) {
+      setProducts([]);
+      console.log(err);
+    }
+  };
+
+  const updateFilteredProducts = (_products, _filter) => {
+    if (filterSelected === "Todos") {
+      setFilteredProducts(_products);
+    } else {
+      setFilteredProducts(
+        _products.filter((product) => product.category === _filter)
+      );
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [filterSelected]);
 
     return (
-        <section>
-            <div className="container">
-                <div >
-                    <h2 >Nuestros artículos</h2>
-                    <Image src={brushTwo} alt="Paint" className={styles.brush} />
-                </div>
-            </div>
-            <div >
-          <aside >
-          <ul >
-              
-            <li>
-                <h1>list</h1>
-            </li>
-            </ul>
-          </aside>
-          <div >
-            <ol >
-            <div>
-               <CardProduct/>
-            </div>
-            </ol>
+        <section className={styles.articles}>
+        <div className="container">
+          <div className={styles.wrapper}>
+            <h2 className={styles.title}>Nuestros artículos</h2>
+            <Image src={brushTwo} alt="Paint" className={styles.brush} />
           </div>
+          <div className={styles.content}>
+            <aside className={styles.aside}>
+            <ul className={styles.list}>
+                {filters.map((filter) => (
+                  <li
+                    key={filter}
+                    className={`${styles.listItem} ${
+                      filterSelected === filter ? styles.listItemActive : null
+                    }`}
+                  >
+                    <button
+                      onClick={handleFilter}
+                      type="button"
+                      value={filter}
+                      className={styles.button}
+                    >
+                      {filter}
+                      <Image
+                        src={arrow}
+                        alt="Flecha"
+                        className={`
+                            ${styles.arrow}
+                            ${
+                              filterSelected === filter
+                                ? styles.arrowActive
+                                : null
+                            }
+                          `}
+                      />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </aside>
+            <div className={styles.articlesWrapper}>
+              <ol className={styles.articlesList}>
+              {filteredProducts.map((product) => (
+                  <CardProduct key={product.id} product={product} />
+                ))}
+              </ol>
+            </div>
           </div>
-        </section>
+        </div>
+      </section>
     );
 };
 
